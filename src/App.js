172fa6a0662b0505b23d8db54c473d99bd06f7f1
didue <jordan.hans7@gmail.com>
2025-02-12 23:14:11 +0900

    [Change] Set Architect

--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-import JSConfetti from 'js-confetti';
+import { Header } from './pages/Header';
+import { Intro } from './pages/Intro';
+import { Footer } from './pages/Footer';
 
 const App = () => {
 
-  
-  const fnExploreConfetti = () => {
-    
-    const confetti = new JSConfetti();
-
-    confetti.addConfetti({
-      emojis: ['🎉', '👰🏻‍♀️', '🤵🏻‍♂️', '✨', '💛', '💜', '💞', '🌸'],
-      emojiSize: 70,
-      confettiNumber: 50,
-   })
-  }
-
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={fnExploreConfetti}>
-          Boom 😈
-        </button>
-      </header>
+    <div className='container'>
+      {/* 헤더 */}
+      <Header/>
+      {/* 인사말 */}
+      <Intro/>
+      {/* 예약일시 */}
+      {/* 갤러리 */}
+      {/* 오시는길 안내 */}
+      {/* 마음 전하실 곳 */}
+      {/* 안내사항 */}
+      {/* 방명록/참석여부 */}
+      {/* 푸터 */}
+      <Footer/>
     </div>
   );
 }
---
+++ src/pages/Intro.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+export const Intro = () => {
+
+    return (
+        <div>
+            인사말
+        </div>
+    )
+}
